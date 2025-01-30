f6338ce1f6f30f19ea807c0dc2da595e13b23596
Ricardo Passinho <passocurto@gmail.com>
2025-01-30 17:10:52 -0300

    config: estlint npmrc

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+    PORT: z.coerce.number().default(3333),
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    // DATABASE_URL: z.string(),
+    // JWT_SECRET: z.string(),
+    // JWT_EXPIRATION: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+    console.error(' ❌ Invalid enviroment variable', _env.error.format);
+
+    throw new Error('Env enviroment variable error');
+}
+
+export const env = _env.data;
