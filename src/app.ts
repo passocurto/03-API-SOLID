f0180bd6238e95b6663f836e744c02e91e5fd611
Ricardo Passinho <passocurto@gmail.com>
2025-01-31 22:27:37 -0300

    insert prisma and docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify();
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        id: '1',
+        name: 'Ricardo Passinho',
+        email: 'passocurto@gmail.com',
+        password: '123456'
+    }
+})
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,5 @@
-import 'dotenv/config';
-import { z } from 'zod';
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
     PORT: z.coerce.number().default(3333),
@@ -12,9 +12,9 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
-    console.error(' ❌ Invalid enviroment variable', _env.error.format);
+    console.error(' ❌ Invalid enviroment variable', _env.error.format)
 
-    throw new Error('Env enviroment variable error');
+    throw new Error('Env enviroment variable error')
 }
 
 export const env = _env.data;
