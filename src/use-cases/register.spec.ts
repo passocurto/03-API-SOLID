40c411c86a512bccc383c93cb2332265ae040980
Ricardo Passinho <passocurto@gmail.com>
2025-02-06 09:59:34 -0300

    add coverage and test:ui

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -52,7 +52,7 @@ describe('Register Use Case', () => {
             password: '123456'
         })
 
-        expect(() => registerUseCase.execute({
+        await expect(() => registerUseCase.execute({
             name: 'John Doe',
             email,
             password: '123456'
